b60be06d1ae3b75e67707bf950e521d71531ea22
atulhiralkar68 <atulhiralkar68@gmail.com>
2024-09-03 17:05:42 +0530

    New Project

---
+++ cypress/Integration/examples/Test1.js
@@ -0,0 +1,6 @@
+describe('My First Test Suite', function(){
+    it('My First TEst Case', function(){
+        //Test Steps
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    })
+})
